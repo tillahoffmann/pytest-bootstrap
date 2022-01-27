584e40f586934d79eb72b27ca50016b0bcf7370f
Till Hoffmann <tillahoffmann@gmail.com>
2022-01-27 18:43:28 -0500

    Add workflow to push to pypi.

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
+import re
 from setuptools import find_packages, setup
+
+
+# Load the README and make modifications for unsupported sphinx commands.
+with open('README.rst') as fp:
+    long_description = fp.read()
+long_description = long_description \
+    .replace('.. doctest::', '.. code-block:: python') \
+    .replace(':math:', ':code:')
+long_description = re.sub(r'(\.\. automodule:: .*?$)|(\.\. toctree::)|(\.\. plot::)', r':code:`\1`',
+                          long_description, flags=re.MULTILINE)
+
+
+# Load the version number
+try:
+    with open('VERSION') as fp:
+        version = fp.read().strip()
+except FileNotFoundError:
+    version = 'dev'
 
 
 setup(
     name='pytest-bootstrap',
     packages=find_packages(),
-    version='0.1.0',
+    version=version,
     install_requires=[
         'numpy',
     ],
@@ -23,4 +42,6 @@ setup(
     classifiers=[
         'Framework :: Pytest',
     ],
+    long_description_content_type="text/x-rst",
+    long_description=long_description,
 )
